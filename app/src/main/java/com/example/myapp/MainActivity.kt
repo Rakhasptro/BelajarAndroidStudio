9a5729dde792077c33fc66d449cba362c2bc5c9f
Rakha adi <rakhaadi24@gmail.com>
2025-09-22 22:58:51 +0700

    pertemuan1

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var angkaBulat: Int = 0
+    fun hitung() {
+        angkaBulat++
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView)
+        val btnHitung: Button = findViewById(R.id.tombol_hitung)
+
+        btnHitung.setOnClickListener {
+            hitung()
+            textView.text = angkaBulat.toString()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
